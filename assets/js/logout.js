621ce18e2ebaab603e312ef3b4bc94f6e1be01e3
GreenJeans <40717979+groscoe42@users.noreply.github.com>
2018-10-01 12:21:03 -0400

    login working git add . !

---
+++ assets/js/logout.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+//logout stuff
+var mainApp = {};
+
+(function(){
+    var firebase = firebase_app;
+    var uid = null;
+    
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      uid = user.uid;
+    }else{
+        //redirect to login page
+        uid = null;
+        window.location.replace("index.html");
+    }
+  });
+
+  function logOut(){
+      firebase.auth().signOut();
+  }
+
+  mainApp.logOut = logOut;
+
+})()
+});
